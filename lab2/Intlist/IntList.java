91e01781adcc14865dd2b4dbb4ada5977dc9c9ef
Qiyan(Ryan) Yu <31525880+QiyanYu@users.noreply.github.com>
2022-03-11 14:52:54 -0800

    try recursive version

--- lab2/Intlist/IntList.java
+++ lab2/Intlist/IntList.java
@@ -101,6 +101,15 @@ public class IntList {
      * * elements of B.  May NOT modify items of A.  Use 'new'.
      */
     public static IntList catenate(IntList A, IntList B) {
+        if (A == null) {
+            return B;
+        }
+
+        return new IntList(A.first, catenate(A.rest, B));
+
+        /**
+         * This is the iterative version;
+
         IntList ret = null;
         IntList p;
         if (A != null) {
@@ -133,6 +142,7 @@ public class IntList {
         }
 
         return ret;
+         */
     }
 
 
