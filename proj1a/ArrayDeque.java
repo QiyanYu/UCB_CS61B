fd3930782c4258bdceebe40e4fb18adacb9ce9a4
Qiyan(Ryan) Yu <31525880+QiyanYu@users.noreply.github.com>
2022-03-21 22:27:53 -0700

    fix bug

--- proj1a/ArrayDeque.java
+++ proj1a/ArrayDeque.java
@@ -72,7 +72,7 @@ public class ArrayDeque<T> {
     }
 
     public T removeFirst() {
-        if (size <= items.length / 4) {
+        if (size <= items.length / 4 && items.length >= 16) {
             resize(items.length / 2);
         }
         if (size <= 0) {
@@ -86,7 +86,7 @@ public class ArrayDeque<T> {
     }
 
     public T removeLast() {
-        if (size <= items.length / 4) {
+        if (size <= items.length / 4 && items.length >= 16) {
             resize(items.length / 2);
         }
         if (size <= 0) {
