150f2a85cc39864d1d418d9e2029035614eaf62e
Qiyan(Ryan) Yu <31525880+QiyanYu@users.noreply.github.com>
2022-03-23 23:06:44 -0700

    add more tests

--- proj1b/TestOffByOne.java
+++ proj1b/TestOffByOne.java
@@ -1,4 +1,5 @@
 import org.junit.Test;
+
 import static org.junit.Assert.*;
 
 public class TestOffByOne {
@@ -11,8 +12,11 @@ public class TestOffByOne {
     public void testOffByOneComparator() {
         assertTrue(offByOne.equalChars('a', 'b'));
         assertTrue(offByOne.equalChars('r', 'q'));
+        assertTrue(offByOne.equalChars('R', 'Q'));
+        assertTrue(offByOne.equalChars('&', '%'));
         assertFalse(offByOne.equalChars('a', 'e'));
         assertFalse(offByOne.equalChars('z', 'a'));
         assertFalse(offByOne.equalChars('a', 'a'));
+        assertFalse(offByOne.equalChars('a', 'B'));
     }
 }
