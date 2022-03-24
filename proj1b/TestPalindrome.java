c883ca1371b6aea1080641be9d268a7d649448bc
Qiyan(Ryan) Yu <31525880+QiyanYu@users.noreply.github.com>
2022-03-23 22:58:57 -0700

    finish task 3

--- proj1b/TestPalindrome.java
+++ proj1b/TestPalindrome.java
@@ -1,8 +1,9 @@
 import org.junit.Test;
+
 import static org.junit.Assert.*;
 
 public class TestPalindrome {
-    /*// You must use this palindrome, and not instantiate
+    // You must use this palindrome, and not instantiate
     // new Palindromes, or the autograder might be upset.
     static Palindrome palindrome = new Palindrome();
 
@@ -14,5 +15,48 @@ public class TestPalindrome {
             actual += d.removeFirst();
         }
         assertEquals("persiflage", actual);
-    } Uncomment this class once you've created your Palindrome class. */
+    }
+
+    @Test
+    public void testIsPalindrome() {
+        String t1 = "noon";
+        assertTrue(palindrome.isPalindrome(t1));
+
+        String t2 = "cat";
+        assertFalse(palindrome.isPalindrome(t2));
+
+        String t3 = "a";
+        assertTrue(palindrome.isPalindrome(t3));
+
+        String t4 = "Aa";
+        assertFalse(palindrome.isPalindrome(t4));
+
+        String t5 = "";
+        assertTrue(palindrome.isPalindrome(t5));
+
+        String t6 = "abcba";
+        assertTrue(palindrome.isPalindrome(t6));
+    }
+
+    @Test
+    public void testIsPalindrome2() {
+        CharacterComparator cc = new OffByOne();
+        String t1 = "abcb";
+        assertTrue(palindrome.isPalindrome(t1, cc));
+
+        String t2 = "abba";
+        assertFalse(palindrome.isPalindrome(t2, cc));
+
+        String t3 = "a";
+        assertTrue(palindrome.isPalindrome(t3, cc));
+
+        String t4 = "Ab";
+        assertFalse(palindrome.isPalindrome(t4, cc));
+
+        String t5 = "";
+        assertTrue(palindrome.isPalindrome(t5, cc));
+
+        String t6 = "flake";
+        assertTrue(palindrome.isPalindrome(t6, cc));
+    }
 }
