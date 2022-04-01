263f4ae97ec1b2173d9dedb638b37dc02832ac4b
Qiyan(Ryan) Yu <31525880+QiyanYu@users.noreply.github.com>
2022-04-01 08:55:57 -0700

    lab 5

--- proj2/byog/lab5/HexWorld.java
+++ proj2/byog/lab5/HexWorld.java
@@ -1,15 +1,49 @@
 package byog.lab5;
-import org.junit.Test;
-import static org.junit.Assert.*;
 
-import byog.TileEngine.TERenderer;
 import byog.TileEngine.TETile;
-import byog.TileEngine.Tileset;
 
 import java.util.Random;
 
-/**
- * Draws a world consisting of hexagonal regions.
- */
 public class HexWorld {
+    public void addHexagon() {
+
+    }
+
+    public static int hexRowWidth(int s, int i) {
+        int effectiveI = i;
+        if (i >= s) {
+            effectiveI = 2 * s - 1 - effectiveI;
+        }
+        return s + 2 * effectiveI;
+    }
+
+    public static int hexRowOffset(int s, int i) {
+        int effectiveI = i;
+        if (i >= s) {
+            effectiveI = 2 * s - 1 - effectiveI;
+        }
+        return -effectiveI;
+    }
+
+
+    public static void addRow(TETile[][] world, Position p, int width, TETile t) {
+        for (int xi = 0; xi < width; xi++) {
+            int xCoord = p.x + xi;
+            int yCoord = p.y;
+            world[xCoord][yCoord] = TETile.colorVariant(t, 32, 32, 32, new Random());
+        }
+    }
+
+    public static void addHexagon(TETile[][] world, Position p, int s, TETile t) {
+        if (s < 2) {
+            throw new IllegalArgumentException("Hexagon must be at least size 2");
+        }
+        for (int yi = 0; yi < 2 * s; yi += 1) {
+            int thisRowY = p.y + yi;
+            int xRowStart = p.x + hexRowOffset(s, yi);
+            Position rowStartP = new Position(xRowStart, thisRowY);
+            int rowWidth = hexRowWidth(s, yi);
+            addRow(world, rowStartP, rowWidth, t);
+        }
+    }
 }
