69ae0dc4809b7dd7f2e0add7a220fff78a7460e1
Qiyan(Ryan) Yu <31525880+QiyanYu@users.noreply.github.com>
2022-04-19 09:10:22 -0700

    initial MapGenerationParameters.java

---
+++ proj2/byog/Core/MapGenerationParameters.java
@@ -0,0 +1,10 @@
+package byog.Core;
+
+public class MapGenerationParameters {
+    public int width;
+    public int height;
+
+    public static MapGenerationParameters defaultParameters(){
+        return new MapGenerationParameters();
+    }
+}
