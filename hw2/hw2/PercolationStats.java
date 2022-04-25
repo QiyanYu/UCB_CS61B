8fb933f568466722be210b9a0778c65ca5ba289c
Qiyan(Ryan) Yu <31525880+QiyanYu@users.noreply.github.com>
2022-04-25 14:12:11 -0700

    PercolationStats.java skeleton code

--- hw2/hw2/PercolationStats.java
+++ hw2/hw2/PercolationStats.java
@@ -1,5 +1,49 @@
 package hw2;
 
 public class PercolationStats {
+    private int N;
+    private int T;
+    private PercolationFactory pf;
 
+    public PercolationStats(int N, int T, PercolationFactory pf) {
+        if (N <= 0 || T <= 0) {
+            throw new java.lang.IllegalArgumentException("Invalid N or T!");
+        }
+        this.N = N;
+        this.T = T;
+        this.pf = pf;
+    }
+
+    /**
+     * sample mean of percolation threshold
+     * public double mean() {
+     * return -1.0;
+     * }
+     * <p>
+     * /**
+     * sample standard deviation of percolation threshold
+     *
+     * @return
+     */
+    public double stddev() {
+        return -1.0;
+    }
+
+    /**
+     * low endpoint of 95% confidence interval
+     *
+     * @return
+     */
+    public double confidenceLow() {
+        return -1.0;
+    }
+
+    /**
+     * high endpoint of 95% confidence interval
+     *
+     * @return
+     */
+    public double confidenceHigh() {
+        return -1.0;
+    }
 }
